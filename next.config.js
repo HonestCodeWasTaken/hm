c86a2c692abe1cdce1c1797ffe5679529d8259d2
honestdev <honestdev@yahoo.com>
2022-11-20 18:14:56 +0200

    config changes?

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable import/extensions */
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'))
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
