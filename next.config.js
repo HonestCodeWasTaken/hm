d5e684d16c8e33f8fa1409c99a76c4843a304cc4
honestdev <honestdev@yahoo.com>
2022-11-20 18:18:35 +0200

    dunno

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable import/extensions */
-!process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'))
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
